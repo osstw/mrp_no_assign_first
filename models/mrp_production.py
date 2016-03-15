9b7c4c985c2c6c92cdae4d8fb14f72d2683071b4
wangting <39181819@qq.com>
2016-03-15 16:25:41 +0800

    [FIX]

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -8,3 +8,14 @@ class Production(models.Model):
     @api.multi
     def test_ready(self):
         return True
+
+    @api.multi
+    def get_assigned_count(self, product_id):
+        self.ensure_one()
+        if not isinstance(product_id, int):
+            return None
+
+        move = self.move_lines.filtered(lambda m: m.product_id.id == product_id)
+        move.ensure_one()
+        if move:
+            return sum(quant.qty for quant in move.reserved_quant_ids)
